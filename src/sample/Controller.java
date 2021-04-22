141b45b44a0ebd42a15d47bd67d749d0f5949d99
crhughey <crhughey11@gmail.com>
2021-04-22 18:45:11 -0500

    All the windows are formatted and useable, I just need to wire the buttons to their own methods now.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,16 +14,48 @@ public class Controller {
     Button btnStudentSignIn;
     @FXML
     Button btnProfessorSignIn;
+    @FXML
+    Button btnStudentSignUp;
+    @FXML
+    Button btnProfessorSignUp;
+    @FXML
+    Button btnCancel;
 
+
+    //Navigation Buttons
+    //This button takes the user from the startup screen to the student Sign In page
     public void studentSignInPressed() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("StudentSignIn.fxml"));
         Stage window = (Stage) btnStudentSignIn.getScene().getWindow();
         window.setScene(new Scene(root, 600, 600));
     }
 
+    //This button take the user from startup to the student sign up page
+    public void studentSignUpPressed() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StudentSignUp.fxml"));
+        Stage window = (Stage) btnStudentSignUp.getScene().getWindow();
+        window.setScene(new Scene(root, 600, 600));
+    }
+
+    //This button take the user from startup to the professor sign in page
     public void professorSignInPressed() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ProfessorSignIn.fxml"));
         Stage window = (Stage) btnProfessorSignIn.getScene().getWindow();
         window.setScene(new Scene(root, 600, 600));
     }
+
+    //This button takes the user from startup to the professor sign up page
+    public void professorSignUpPressed () throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("ProfessorSignUp.fxml"));
+        Stage window = (Stage) btnProfessorSignUp.getScene().getWindow();
+        window.setScene(new Scene(root, 600, 600));
+    }
+
+    //This button takes the user back to the startup page from any of the sign in, or sign up pages
+    public void cancelButtonPressed() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Stage window = (Stage) btnCancel.getScene().getWindow();
+        window.setScene(new Scene(root, 600, 600));
+    }
+
 }
